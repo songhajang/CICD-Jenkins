0381004bb0ac3a62c4aa8d6a5529f843dac62757
menzzi <minmin3927@naver.com>
2025-09-15 16:50:12 +0900

    feat : spring maven

---
+++ CI_CD_OS_maven/src/main/java/edu/ce/fisa/CiCdOsMavenApplication.java
@@ -0,0 +1,13 @@
+package edu.ce.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdOsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdOsMavenApplication.class, args);
+	}
+
+}
