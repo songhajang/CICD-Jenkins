a76990f954b2c3e86bcdd78a8291413c7514d757
songhajang <angpan2004@gmail.com>
2025-09-16 10:29:59 +0900

    feat: create gradle project

---
+++ step04_gradleBuild/src/main/java/edu/ce/fisa/Step04GradleBuildApplication.java
@@ -0,0 +1,13 @@
+package edu.ce.fisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step04GradleBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Step04GradleBuildApplication.class, args);
+	}
+
+}
---
+++ step04_gradleBuild/src/main/java/edu/ce/fisa/controller/Controller.java
@@ -0,0 +1,19 @@
+package edu.ce.fisa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/get")
+	public String getFn() {
+		return "Get 방식 테스트입니당 :)";
+	}
+	
+	@PostMapping("/post")
+	public String postFn() {
+		return "POST 방식 테스트입니당 :)";
+	}
+}
