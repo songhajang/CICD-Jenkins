4869ae1c455c0090cdc5de5ab8096834d9d77344
JANG SONG HA <87272634+songhajang@users.noreply.github.com>
2025-09-18 10:30:16 +0900

    fix:controller 업데이트 멘트 수정

--- step04_gradleBuild/src/main/java/edu/ce/fisa/controller/Controller.java
+++ step04_gradleBuild/src/main/java/edu/ce/fisa/controller/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 
 	@GetMapping("/get")
 	public String getFn() {
-		return "Get 방식 테스트입니당 :) 업데이트 테스트!!! 1";
+		return "Get 방식 테스트입니당 :) 업데이트 테스트!!! 2";
 	}
 	
 	@PostMapping("/post")
